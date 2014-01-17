581543f989dac66824aa861ffae77c88e78e06f9
ririzarr <rairizarry@gmail.com>
2014-01-17 16:51:52 -0500

    adding a dir with scripts to make example datasets

---
+++ Rscripts/microarraydata-eda-lab.R
@@ -0,0 +1,17 @@
+library(SpikeIn)
+data("SpikeIn95")
+##from previous data exploration we know that array 55 is bad. we pick
+##two groups with same  52-55 and 56-59
+int=pm(SpikeIn95)[,52:59] ###int for intensity
+spikeInDesign=pData(SpikeIn95)[52:59,]
+
+cdfname <- getCdfInfo(SpikeIn95)
+psets <- as.list(cdfname)
+psets <- psets[order(names(psets))]
+index <- unlist(sapply(psets, function(x) x[, 1]), use.names = FALSE)
+locations <- i2xy(index)
+
+save(int,spikeInDesign,locations,file="eda-lab-data-microarrays.rda")
+
+
+
