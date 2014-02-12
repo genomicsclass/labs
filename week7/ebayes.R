872e44742be6cf38d10cd53cba440bab4545d23f
Mike Love <mikelove1@gmail.com>
2014-02-11 21:56:03 -0500

    little more work on ebayes
    Former-commit-id: 27e24883da1024b1c06374c5d25ba1f7cd9cd508

--- week7/ebayes.R
+++ week7/ebayes.R
@@ -22,5 +22,15 @@ ebfit <- ebayes(fit)
 limmares <- data.frame(dm=coef(fit)[,"fac2"], p.value=ebfit$p.value[,"fac2"])
 with(limmares, plot(dm, -log10(p.value),
      cex=.5, col=cols,
-     xlim=c(-1,1), ylim=c(0,4)))
-abline(h=2,lty=2)
+     xlim=c(-1,1), ylim=c(0,5)))
+abline(h=2,v=c(-.2,.2), lty=2)
+
+
+n <- 40
+qs <- seq(from=0,to=.2,length=n)
+idx <- sapply(seq_len(n),function(i) which(as.integer(cut(tt$s^2,qs)) == i)[1])
+idx <- idx[!is.na(idx)]
+plot(1,1,xlim=c(0,.17),ylim=c(0,1),type="n",
+     xlab="",ylab="")
+segments((tt$s^2)[idx],rep(.1,n),
+         ebfit$s2.post[idx],rep(.9,n))
