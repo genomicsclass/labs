73e41d6bc259b084fbc0a3b473d2bd51389cacfd
Mike Love <michaelisaiahlove@gmail.com>
2014-02-12 12:23:39 -0500

    work on wednesday

--- week7/ebayes.R
+++ week7/ebayes.R
@@ -5,15 +5,18 @@ library(genefilter)
 fac <- factor(rep(1:2,each=3))
 tt <- rowttests(exprs(rma95),fac)
 mask <- with(tt, abs(dm) < .2 & p.value < .01)
-cols <- ifelse(mask,"red","black")
-with(tt, plot(dm, -log10(p.value), cex=.5,
+spike <- rownames(rma95) %in% colnames(pData(rma95))
+cols <- ifelse(mask,"red",ifelse(spike,"green","black"))
+
+with(tt, plot(-dm, -log10(p.value), cex=.5,
      xlim=c(-1,1), ylim=c(0,5),
+     xlab="difference in means",
      col=cols))
 abline(h=2,v=c(-.2,.2), lty=2)
 
-tt$s <- tt$dm / tt$statistic
-with(tt, plot(-log10(p.value), s,
-              log="y",
+tt$s <- apply(exprs(rma95), 1, function(row) sqrt(.5 * (var(row[1:3]) + var(row[4:6]))))
+with(tt, plot(s, -log10(p.value),
+              log="x",xlab="estimate of standard deviation",
               cex=.5, col=cols))
 
 library(limma)
@@ -21,7 +24,7 @@ fit <- lmFit(rma95, model.matrix(~ fac))
 ebfit <- ebayes(fit)
 limmares <- data.frame(dm=coef(fit)[,"fac2"], p.value=ebfit$p.value[,"fac2"])
 with(limmares, plot(dm, -log10(p.value),
-     cex=.5, col=cols,
+     cex=.5, col=cols,xlab="difference in means",
      xlim=c(-1,1), ylim=c(0,5)))
 abline(h=2,v=c(-.2,.2), lty=2)
 
@@ -30,7 +33,10 @@ n <- 40
 qs <- seq(from=0,to=.2,length=n)
 idx <- sapply(seq_len(n),function(i) which(as.integer(cut(tt$s^2,qs)) == i)[1])
 idx <- idx[!is.na(idx)]
-plot(1,1,xlim=c(0,.17),ylim=c(0,1),type="n",
-     xlab="",ylab="")
+par(mar=c(5,5,2,2))
+plot(1,1,xlim=c(0,.21),ylim=c(0,1),type="n",
+     xlab="variance estimates",ylab="",yaxt="n")
+axis(2,at=c(.1,.9),c("before","after"),las=2)
 segments((tt$s^2)[idx],rep(.1,n),
          ebfit$s2.post[idx],rep(.9,n))
+
