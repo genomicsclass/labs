e9dbed571fc9884aaef77b25d9b231913d89620e
Mike Love <mikelove1@gmail.com>
2014-02-11 19:09:09 -0500

    ttest vs ebayes
    Former-commit-id: e709299c186d2ec1ee1c71c6fd39ca02475fe35f

--- week7/ebayes.R
+++ week7/ebayes.R
@@ -1,1 +1,26 @@
-biocLite("SpikeInSubset")
+# biocLite("SpikeInSubset")
+library(SpikeInSubset)
+data(rma95)
+library(genefilter)
+fac <- factor(rep(1:2,each=3))
+tt <- rowttests(exprs(rma95),fac)
+mask <- with(tt, abs(dm) < .2 & p.value < .01)
+cols <- ifelse(mask,"red","black")
+with(tt, plot(dm, -log10(p.value), cex=.5,
+     xlim=c(-1,1), ylim=c(0,5),
+     col=cols))
+abline(h=2,v=c(-.2,.2), lty=2)
+
+tt$s <- tt$dm / tt$statistic
+with(tt, plot(-log10(p.value), s,
+              log="y",
+              cex=.5, col=cols))
+
+library(limma)
+fit <- lmFit(rma95, model.matrix(~ fac))
+ebfit <- ebayes(fit)
+limmares <- data.frame(dm=coef(fit)[,"fac2"], p.value=ebfit$p.value[,"fac2"])
+with(limmares, plot(dm, -log10(p.value),
+     cex=.5, col=cols,
+     xlim=c(-1,1), ylim=c(0,4)))
+abline(h=2,lty=2)
