b6e97a89f2d0e45c5edb45bc8e2267873d9ab068
Mike Love <michaelisaiahlove@gmail.com>
2014-02-13 10:25:40 -0500

    changing colors

--- week7/ebayes.R
+++ week7/ebayes.R
@@ -6,25 +6,25 @@ fac <- factor(rep(1:2,each=3))
 tt <- rowttests(exprs(rma95),fac)
 mask <- with(tt, abs(dm) < .2 & p.value < .01)
 spike <- rownames(rma95) %in% colnames(pData(rma95))
-cols <- ifelse(mask,"red",ifelse(spike,"green","black"))
+cols <- ifelse(mask,"red",ifelse(spike,"dodgerblue","black"))
 
-with(tt, plot(-dm, -log10(p.value), cex=.5,
+with(tt, plot(-dm, -log10(p.value), cex=.8, pch=16,
      xlim=c(-1,1), ylim=c(0,5),
      xlab="difference in means",
      col=cols))
 abline(h=2,v=c(-.2,.2), lty=2)
 
 tt$s <- apply(exprs(rma95), 1, function(row) sqrt(.5 * (var(row[1:3]) + var(row[4:6]))))
-with(tt, plot(s, -log10(p.value),
+with(tt, plot(s, -log10(p.value), cex=.8, pch=16,
               log="x",xlab="estimate of standard deviation",
-              cex=.5, col=cols))
+              col=cols))
 
 library(limma)
 fit <- lmFit(rma95, model.matrix(~ fac))
 ebfit <- ebayes(fit)
 limmares <- data.frame(dm=coef(fit)[,"fac2"], p.value=ebfit$p.value[,"fac2"])
-with(limmares, plot(dm, -log10(p.value),
-     cex=.5, col=cols,xlab="difference in means",
+with(limmares, plot(dm, -log10(p.value),cex=.8, pch=16,
+     col=cols,xlab="difference in means",
      xlim=c(-1,1), ylim=c(0,5)))
 abline(h=2,v=c(-.2,.2), lty=2)
 
