fc85407bb2187ae0b4c8eb89ba2d31318cd6a969
Mike Love <michaelisaiahlove@gmail.com>
2014-04-24 10:46:02 -0400

    adding packages list
    Former-commit-id: 2b665426d3d7dee02edadad224b20a98accfa8dc

---
+++ packages.R
@@ -0,0 +1,101 @@
+# For all weeks:
+
+install.packages("devtools")
+biocLite("devtools")
+install_github("rafalib","ririzarr")
+install_github("dagdata","genomicsclass")
+
+source("http://bioconductor.org/biocLite.R")
+biocLite()
+biocLite("Biobase")
+biocLite("GenomicRanges")
+
+# Week 1
+
+install.packages("dplyr")
+biocLite("Biobase")
+
+# Week 2
+
+biocLite("GEOquery")
+biocLite("parathyroidSE")
+install.packages("matrixStats")
+install.packages("RColorBrewer")
+biocLite("affy")
+biocLite("oligo")
+
+# Week 3
+
+biocLite("genefilter")
+biocLite("limma")
+install.packages("animation")
+
+# Week 4
+
+biocLite("SpikeInSubset")
+biocLite("BackgroundExperimentYeast")
+biocLite("SpikeIn")
+install.packages("MASS")
+biocLite("parathyroidSE")
+install.packages("matrixStats")
+biocLite("limma")
+biocLite("affy")
+biocLite("SpikeIn")
+install.packages("animation")
+biocLite("preprocessCore")
+biocLite("vsn")
+biocLite("affy")
+biocLite("mycAffyData")
+install.packages("SQN")
+
+# Week 5
+
+install.packages("class")
+install.packages("caret")
+install.packages("RColorBrewer")
+biocLite("genefilter")
+install.packages("gplots")
+install.packages("matrixStats")
+install.packages("animation")
+biocLite("yeastCC")
+install.packages("UsingR")
+
+# Week 6
+ 
+biocLite("genefilter")
+install.packages("MASS")
+biocLite("hgfocus.db")
+biocLite("sva")
+biocLite("limma")
+
+# Week 7
+
+biocLite("GEOquery")
+biocLite("limma")
+biocLite("org.Hs.eg.db")
+biocLite("GO.db")
+biocLite("SpikeInSubset")
+biocLite("genefilter")
+biocLite("rae230a.db")
+biocLite("AnnotationDbi")
+biocLite("biomaRt")
+
+# Week 8
+
+biocLite("pasillaBamSubset")
+biocLite("TxDb.Dmelanogaster.UCSC.dm3.ensGene")
+biocLite("Rsamtools")
+biocLite("GenomicAlignments")
+biocLite("DESeq2")
+biocLite("biomaRt")
+biocLite("Gviz")
+biocLite("ggbio")
+
+# For building data objects
+
+Bioconductor:
+
+biocLite("GEOquery")
+biocLite("affy")
+biocLite("simpleaffy")
+biocLite("SpikeIn")
