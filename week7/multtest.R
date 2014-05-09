3b21a2ebabbc93b38df6e9315145767821aaee2b
Mike Love <michaelisaiahlove@gmail.com>
2014-05-09 11:54:34 -0400

    fixing week 6

--- week7/multtest.R
+++ week7/multtest.R
@@ -1,3 +1,13 @@
+---
+layout: page
+title: Multiple testing
+---
+
+```{r options, echo=FALSE}
+opts_chunk$set(fig.path=paste0("figure/", sub("(.*).Rmd","\\1",basename(knitr:::knit_concord$get('infile'))), "-"))
+```
+
+```{r}
 drawU <- function(a,y,boxes=FALSE,xmax=1) {
   plot(1,1,type="n",xlim=c(0,xmax),ylim=c(0,1.2*y),bty="L",xlab="p",ylab="",las=1)
   lines(c(0,1,1,0,0),c(0,0,y,y,0))
@@ -8,45 +18,62 @@ drawU <- function(a,y,boxes=FALSE,xmax=1) {
     segments(0:x/x, 0, 0:x/x, y)
   }
 }
+```
 
+```{r}
 drawU(.3,1)
 drawU(1/20,1)
 drawU(1/20,20000/20,TRUE)
 drawU(1/100,20000/100,TRUE)
 drawU(1/1000,20000/1000,TRUE,.01)
+```
 
+```{r}
 z <- rnorm(100)
 brks <- 0:20/20
 hist(pnorm(z),col="grey",main="",xlab="p",breaks=brks)
+```
 
+```{r}
 z <- rnorm(10000)
 hist(pnorm(z),col="grey",main="",xlab="p",breaks=brks)
+```
 
+```{r}
 z <- c(rnorm(10000), rep(-3.72,500))
 h <- hist(pnorm(z),col="grey",main="",xlab="p",breaks=brks)
 abline(h=10000/20,col="red",lwd=4,lty=3)
 abline(h=h$counts[1],col="blue",lwd=4,lty=3)
+```
 
+```{r}
 h <- hist(pnorm(z),col="grey",main="",xlab="p",breaks=0:50/50)
 abline(h=10000/50,col="red",lwd=4,lty=3)
 abline(h=h$counts[1],col="blue",lwd=4,lty=3)
+```
 
+```{r}
 h <- hist(pnorm(z),col="grey",main="",xlab="p",breaks=0:100/100)
 abline(h=10000/100,col="red",lwd=4,lty=3)
 abline(h=h$counts[1],col="blue",lwd=4,lty=3)
+```
 
+```{r}
 h <- hist(pnorm(z),col="grey",main="",xlab="p",breaks=0:10000/10000)
 abline(h=10000/10000,col="red",lwd=4,lty=3)
 abline(h=sum(h$counts[1]),col="blue",lwd=4,lty=3)
+```
 
-
+```{r}
 set.seed(1)
 pvals <- c(runif(90),runif(10,0,.001))
 plot(sort(pvals),xlab="i",ylab="p-value",ylim=c(0,1))
 abline(0, .05/length(pvals))
 legend("top",expression(slope~alpha/m))
+```
 
+```{r}
 plot(sort(pvals),xlab="i",ylab="p-value",ylim=c(0,.03),xlim=c(0,13))
 abline(0, .05/length(pvals))
 legend("top",expression(slope~alpha/m))
-
+```
