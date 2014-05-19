cb2e299c5c6dce2de5bd15102a97e6230891dcf3
Mike Love <michaelisaiahlove@gmail.com>
2014-05-19 13:56:35 -0400

    adding auto footnotes maker

---
+++ footnotes.R
@@ -0,0 +1,18 @@
+out <- c("---", "layout: page",
+         "title: Collated footnotes for Data Analysis for Genomics",
+         "---","")
+dirs <- list.files(".","week*")
+for (dir in dirs) {
+  files <- list.files(dir, "*.Rmd")
+  for (file in files) {
+    lines <- readLines(paste0(dir,"/",file))
+    if (sum(grepl("## Footnotes",lines)) == 0) next 
+    title <- sub("title: (.*)","\\1",grep("title:",lines,value=TRUE)[1])
+    cat("writing:",dir,"/",title,"\n")
+    footidx <- grep("## Footnotes", lines)
+    footnotes <- lines[(footidx+1):length(lines)]
+    footnotes <- footnotes[footnotes != ""]
+    out <- c(out, paste0("# ",title), footnotes,"")
+  }
+}
+writeLines(out, con="footnotes.md")
